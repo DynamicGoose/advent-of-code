2d451f85c858e08cd584d3b40ce51a564afb0a55
Géza Ahsendorf <gezaahs@gmail.com>
2024-12-03 21:05:07 +0100

    day_1_2

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -12,6 +12,11 @@ fn main() {
         list_2.push(values[1].parse().unwrap());
     });
 
+    part_one(list_1.to_owned(), list_2.to_owned());
+    part_two(list_1, list_2);
+}
+
+fn part_one(mut list_1: Vec<u32>, mut list_2: Vec<u32>) {
     list_1.sort_unstable();
     list_2.sort_unstable();
 
@@ -23,4 +28,14 @@ fn main() {
     }
 
     println!("{distance}");
+}
+
+fn part_two(list_1: Vec<u32>, list_2: Vec<u32>) {
+    let mut similarity: u32 = 0;
+
+    for n in list_1 {
+        similarity += n * list_2.iter().filter(|m| **m == n).count() as u32;
+    }
+
+    println!("{similarity}")
 }
