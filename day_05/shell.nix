5abbb8c97df156f2ec9a6d63a61471b66a0e1493
Géza Ahsendorf <gezaahs@gmail.com>
2024-12-05 13:45:30 +0100

    Revert "day_05 1"
    This reverts commit b43c5ca5746f71a04715b3ea7ca34f1da2e6ef24.

--- day_05/shell.nix
+++ day_05/shell.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {};
-clangStdenv.mkDerivation {
-  name = "clang-nix-shell";
-  nativeBuildInputs = [clang-tools];
-  buildInputs = [ /* add libraries here */ ];
+
+mkShell {
+  nativeBuildInputs = [libgcc pkg-config];
+  buildInputs = [glibc libgcc];
 }
